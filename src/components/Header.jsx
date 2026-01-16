8dba0a36f586cc76983f236d22dde73239adc5e8
Shreeyans Vichare <76224515+Shreeyans2305@users.noreply.github.com>
2026-01-16 10:16:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     navigate('/');
   }
 
-  const logoImage = isDarkMode ? '/tar3.jpg' : '/tar5.png';
+  const logoImage = isDarkMode ? '/tar3.jpg' : '/tar.jpeg';
 
   return (
     <>
